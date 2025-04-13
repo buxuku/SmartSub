3bc57970230fb296edf3061acf5354d474f26222
linxiaodong <calm.lin@wukongsch.com>
2025-04-13 22:30:29 +0800

    feat: srt format and fix deeplx

--- main/service/deeplx.ts
+++ main/service/deeplx.ts
@@ -1,23 +1,15 @@
 import axios from "axios";
 export default async function deeplx(query, proof) {
-  const { apiKey: apiUrl1, apiSecret: apiUrl2 } = proof || {};
+  const { apiUrl } = proof || {};
   try {
-    const res = await axios.post(apiUrl1, {
-      text: query,
+    const res = await axios.post(apiUrl, {
+      text: query?.join('\n'),
       source_lang: "en", 
       target_lang: "zh",
     });
     return res?.data?.alternatives?.[0] || "";
   } catch (error) {
-    try {
-      const res = await axios.post(apiUrl2, {
-        text: query,
-        source_lang: "en",
-        target_lang: "zh", 
-      });
-      return res?.data?.alternatives?.[0] || "";
-    } catch (error) {
-      return "error";
-    }
+    console.log(error, 'error');
+    throw error;
   }
 }
