5946085a171cf5e5fd3fa27c562c1e95e0026518
buxuku <buxuku@gmail.com>
2026-09-16 14:06:37 +0800

    chore(deps): update iconv-lite version in yarn.lock and add utility function import in EmbeddedSubtitlePanel

--- renderer/components/toolbox/embeddedSubtitles/EmbeddedSubtitlePanel.tsx
+++ renderer/components/toolbox/embeddedSubtitles/EmbeddedSubtitlePanel.tsx
@@ -12,6 +12,7 @@ import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
 import { Label } from '@/components/ui/label';
 import { Checkbox } from '@/components/ui/checkbox';
+import { cn } from 'lib/utils';
 import {
   Select,
   SelectContent,
